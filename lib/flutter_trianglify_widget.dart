5df7886881d093a17cb30553eabb704712d9632d
dddrop <go@dddrop.dev>
2023-02-21 01:33:08 +0900

    Fix export

--- lib/flutter_trianglify_widget.dart
+++ lib/flutter_trianglify_widget.dart
@@ -1,7 +1,4 @@
 library flutter_trianglify_widget;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/palette.dart';
+export 'src/trianglify_widget.dart';
--- test/flutter_trianglify_widget_test.dart
+++ test/flutter_trianglify_widget_test.dart
@@ -1,11 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_trianglify_widget/flutter_trianglify_widget.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final Calculator calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  test('', () {});
 }
