80657751d5d24cc0991601d48d9f7a0428ed6d4b
dddrop <go@dddrop.dev>
2023-02-21 20:09:06 +0900

    Refactor codes

--- lib/src/triangulator/vector_2d.dart
+++ lib/src/triangulator/vector_2d.dart
@@ -6,32 +6,18 @@ class Vector2D {
   final double x;
   final double y;
 
-  Vector2D sub(Vector2D vector) {
-    return Vector2D(x - vector.x, y - vector.y);
-  }
+  Vector2D sub(Vector2D vector) => Vector2D(x - vector.x, y - vector.y);
 
-  Vector2D add(Vector2D vector) {
-    return Vector2D(x + vector.x, y + vector.y);
-  }
+  Vector2D add(Vector2D vector) => Vector2D(x + vector.x, y + vector.y);
 
-  Vector2D mult(double scalar) {
-    return Vector2D(x * scalar, y * scalar);
-  }
+  Vector2D mult(double scalar) => Vector2D(x * scalar, y * scalar);
 
-  double mag() {
-    return sqrt(x * x + y * y);
-  }
+  double mag() => sqrt(x * x + y * y);
 
-  double dot(Vector2D vector) {
-    return x * vector.x + y * vector.y;
-  }
+  double dot(Vector2D vector) => x * vector.x + y * vector.y;
 
-  double cross(Vector2D vector) {
-    return x * vector.y - y * vector.x;
-  }
+  double cross(Vector2D vector) => x * vector.y - y * vector.x;
 
   @override
-  String toString() {
-    return 'Vector2D($x, $y)';
-  }
+  String toString() => 'Vector2D($x, $y)';
 }
